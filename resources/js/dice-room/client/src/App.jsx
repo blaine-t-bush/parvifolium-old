fa4be38b4761eeb52b7a67c399d3ff554a3fde91
Blaine Bush <blaine.bush@gmail.com>
2021-04-12 10:37:17 -0700

    Update dice room

---
+++ resources/js/dice-room/client/src/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./App.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Room from "./Room";
+
+const App = () => <Room />;
+
+ReactDOM.render(<App />, document.getElementById("app"));
